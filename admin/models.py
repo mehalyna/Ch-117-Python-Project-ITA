5553e40f30325254b897d8cf768b1653b0d89e29
Artem <artem@gmail.com>
2021-03-29 16:44:56 +0300

    #41-Create models to work with MongoDB

--- admin/models.py
+++ admin/models.py
@@ -1,10 +1,75 @@
-from mongoengine import Document, StringField, EmailField
+from datetime import datetime
+from mongoengine import DateTimeField, Document, EmailField, EmbeddedDocument, EmbeddedDocumentField, FloatField, \
+    IntField, ListField, ReferenceField, StringField
 
 
-class Users(Document):
-    firstname = StringField(max_length=50, required=True)
-    lastname = StringField(max_length=50, required=True)
+class Status:
+    ACTIVE = 'active'
+    DELETED = 'deleted'
+
+
+class Role:
+    USER = 'user'
+    MODERATOR = 'moderator'
+    ADMIN = 'admin'
+
+
+class Preference(EmbeddedDocument):
+    genres = ListField(default=[])
+    authors = ListField(default=[])
+    rating = FloatField(default=2.5, min_value=0.0, max_value=5.0)
+    years = ListField(default=(), max_length=2)
+
+
+class User(Document):
+    firstname = StringField(max_length=100, min_length=1, required=True)
+    lastname = StringField(max_length=100, min_length=1, required=True)
     email = EmailField(required=True, unique=True)
-    password = StringField(required=True)
-    role = StringField(required=True)
-    status = StringField(required=True, default='active')
+    login = StringField(required=True)
+    password = StringField(required=True, min_length=8)
+    role = StringField(default=Role.USER)
+    status = StringField(default=Status.ACTIVE)
+    last_login = DateTimeField(default=datetime.now)
+    reviews = ListField(default=[])
+    recommended_books = ListField(default=[])
+    wishlist = ListField(default=[])
+    preference = EmbeddedDocumentField(Preference.__name__, default=Preference())
+
+
+class BookStatistic(EmbeddedDocument):
+    rating = FloatField(default=2.5, min_value=0.0, max_value=5.0)
+    total_read = IntField(default=0, min_value=0)
+    reading_now = IntField(default=0, min_value=0)
+
+
+class Author(Document):
+    name = StringField(default='', max_length=500)
+    birthdate = StringField(default='', max_length=15)
+    death_date = StringField(default='', max_length=15)
+    status = StringField(default=Status.ACTIVE, max_length=50)
+    books = ListField(default=[])
+
+
+class Book(Document):
+    title = StringField(default='', max_length=200)
+    author_id = ReferenceField(Author.__name__)
+    year = StringField(default='', max_length=20)
+    publisher = StringField(default='', max_length=200)
+    language = StringField(default='', max_length=20)
+    description = StringField(default='', max_length=10000)
+    link_img = StringField(default='', max_length=1000)
+    pages = IntField(default=1, min_value=1)
+    genres = ListField(default=[])
+    status = StringField(default=Status.ACTIVE, max_length=100)
+    store_links = ListField(default=[])
+    statistic = EmbeddedDocumentField(BookStatistic.__name__, default=BookStatistic())
+
+
+class Review(Document):
+    user_id = ReferenceField(User.__name__, required=True)
+    book_id = ReferenceField(Book.__name__, required=True)
+    firstname = StringField(default='', max_length=50)
+    lastname = StringField(default='', max_length=50)
+    status = StringField(default=Status.ACTIVE, max_length=100)
+    comment = StringField(default='', max_length=5000)
+    date = DateTimeField(default=datetime.now)
