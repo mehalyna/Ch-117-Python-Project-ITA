ce88c1895150795a7e525052b436ceb669b58db0
Palusha <monday13081@gmail.com>
2021-04-05 11:34:06 +0300

    refactor unique_check duplicate

--- admin/forms.py
+++ admin/forms.py
@@ -8,25 +8,18 @@ from models import Role, Status, User
 EMAIL_PATTERN = r'^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$'
 
 
-def unique_check(column):
+def unique_check(column, update=False):
     message = f'{column.title()} is already taken'
 
     def _unique_check(form, field):
-        if User.objects(__raw__={column: field.data}).first():
+        user = User.objects(__raw__={column: field.data}).first()
+
+        if user and update and user.id != form.user_id.data:
+            raise ValidationError(message)
+        elif user and not update:
             raise ValidationError(message)
 
     return _unique_check
-
-
-def update_unique_check(column):
-    message = f'{column.title()} is already taken'
-
-    def _update_unique_check(form, field):
-        user = User.objects(__raw__={column: field.data}).first()
-        if user and user.id != form['user_id'].data:
-            raise ValidationError(message)
-
-    return _update_unique_check
 
 
 class AddUserForm(FlaskForm):
@@ -50,8 +43,12 @@ class AddUserForm(FlaskForm):
 class UpdateUserForm(AddUserForm):
     user_id = StringField('Id')
     email = EmailField('Email', validators=[
-        DataRequired(), Regexp(regex=EMAIL_PATTERN, message='Invalid email'), update_unique_check('email')])
-    login = StringField('Login', validators=[DataRequired(), update_unique_check('login')])
+        DataRequired(), Regexp(regex=EMAIL_PATTERN, message='Invalid email'),
+        unique_check('email', update=True)
+    ])
+    login = StringField('Login', validators=[
+        DataRequired(), unique_check('login', update=True)
+    ])
     status = SelectField('Status', choices=[
         (Status.ACTIVE, Status.ACTIVE),
         (Status.INACTIVE, Status.INACTIVE)
