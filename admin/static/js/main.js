e5294c03ae1e36b3a37aa9ebc932f466ab4dc9b7
Artemy Penteskul <artemiy.penteskul@gmail.com>
2021-04-06 13:28:38 +0300

    filtering & searching books

--- admin/static/js/main.js
+++ admin/static/js/main.js
@@ -7,15 +7,26 @@ function change_status(url, status) {
 
 $(document).ready(function () {
     $("#myInput").on("keyup", function () {
-        var value = $(this).val().toLowerCase();
+        let value = $(this).val().toLowerCase();
         $("#myTable tr").filter(function () {
             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
         });
     });
 });
 
+
 window.setTimeout(function() {
     $(".alert").fadeTo(500, 0, function (){
       $(this)[0].style.display = 'none'
     })
 }, 2000);
+
+
+$(document).ready(function(){
+  $("#mySearch").on("keyup", function() {
+    let value = $(this).val().toLowerCase();
+    $("#bookTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
