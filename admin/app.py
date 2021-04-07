b56cd59aea450e5fb46b4c6db32361289c7e1ad7
AndriiSkavreniuk <skavreniuk@gmail.com>
2021-04-07 11:37:11 +0300

    set session lifetime

--- admin/app.py
+++ admin/app.py
@@ -2,6 +2,7 @@ import json
 import os
 
 from bson import ObjectId
+from datetime import timedelta
 from dotenv import load_dotenv
 from flask import flash, Flask, redirect, render_template, request, url_for, session
 from flask_login import LoginManager, logout_user, login_user, login_required
@@ -19,6 +20,7 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['UPLOAD_FOLDER'] = os.getenv('UPLOAD_FOLDER')
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=90)
 
 connect(
     db=os.getenv('DB_NAME'),
@@ -159,6 +161,7 @@ def admin_login():
 def logout():
     logout_user()
     return redirect(url_for('start_page'))
+
 
 @login.user_loader
 def load_user(user_id):
