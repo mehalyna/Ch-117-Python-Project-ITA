c2cc4282c7457b67229ee414ec7c16c62ec4ad62
Palusha <monday13081@gmail.com>
2021-04-11 21:41:07 +0300

    returning to book

--- admin/static/js/main.js
+++ admin/static/js/main.js
@@ -13,3 +13,49 @@ window.setTimeout(function() {
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
+
+function saveValue(e) {
+    var id = e.id;
+    var val = e.value;
+    localStorage.setItem(id, val);
+}
+
+function getSavedValue(v) {
+    if (!localStorage.getItem(v)) {
+        return "";
+    }
+    return localStorage.getItem(v);
+}
+
+(() => {
+    'use strict';
+    const paths_names = ['users_list', 'active_users_list', 'inactive_users_list', 'update_user'];
+    let search = document.getElementById('userSearch');
+    let reset = document.getElementById('searchReset');
+    if (search){
+        search.value = getSavedValue('userSearch');
+    }
+        if (reset){
+            reset.addEventListener('click', () => {
+            localStorage.removeItem('userSearch');
+        })
+        }
+
+    if (!paths_names.includes(window.location.pathname.split('/')[1])) {
+        localStorage.removeItem('userSearch');
+    }
+})();
+
+(() => {
+    'use strict';
+    let edit_button_list = document.querySelectorAll('.save_url');
+    edit_button_list.forEach(button =>{button.addEventListener('click', (event) => {
+        localStorage.removeItem('back_to_url');
+        localStorage.setItem('back_to_url', window.location);
+    }, false);
+    })
+})();
+
+function back_to_list(){
+    window.location.href = localStorage.getItem('back_to_url');
+}
