e827886251ff6fcffe1662d3779c748d4ae3b14b
Palusha <monday13081@gmail.com>
2021-04-16 19:34:33 +0300

    form unique test

--- admin/tests/test_unit/test_forms/test_create_user_form.py
+++ admin/tests/test_unit/test_forms/test_create_user_form.py
@@ -1,6 +1,6 @@
 
 from admin.forms import AddUserForm
-from admin.tests.config import CREATE_VALID_USER, CREATE_INVALID_USER
+from admin.tests.config import CREATE_VALID_USER, CREATE_INVALID_USER, CREATE_LOGGED_USER
 
 
 def test_create_user_form(client):
@@ -18,3 +18,6 @@ def test_create_user_form_with_no_data(client):
     assert not form.validate()
 
 
+def test_create_user_form_unique(client):
+    form = AddUserForm(data=CREATE_LOGGED_USER)
+    assert not form.validate()
