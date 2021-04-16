68e3d65c0a76896ff518e4ec22a2f05aeab00fd1
Radik <rodiontulub@gmail.com>
2021-04-16 13:09:43 +0300

    mongomock is using in all tests now
    in previous version only models test was used mongomock

--- admin/tests/conftest.py
+++ admin/tests/conftest.py
@@ -1,36 +1,45 @@
-import os
-
 import pytest
 from mongoengine import disconnect, connect
 
 from admin.app import create_app
 from admin.tests.test_unit.test_routes.app import App
+from admin.models import User
+from admin.tests.config import (
+    EMAIL, LOGIN, LAST_NAME, FIRST_NAME, PASSWORD, ROLE
+)
 
 
-@pytest.fixture
-def client():
-    disconnect()
-    flask_app = create_app()
-    flask_app.config['TESTING'] = True
-    flask_app.config['WTF_CSRF_ENABLED'] = False
-    with flask_app.test_client() as client:
-        with flask_app.app_context():
-            yield client
-
-
-@pytest.fixture
-def db_client():
-    disconnect()
-    flask_app = create_app()
+@pytest.fixture(scope='session')
+def flask_app():
+    app = create_app()
     disconnect()
     connect('test', host='mongomock://localhost')
-    flask_app.config['TESTING'] = True
-    with flask_app.test_client() as client:
-        with flask_app.app_context():
+    app.config['TESTING'] = True
+    app.config['WTF_CSRF_ENABLED'] = False
+    return app
+
+
+@pytest.fixture(scope='session')
+def flask_app_with_admin(flask_app):
+    u = User()
+    u.firstname = FIRST_NAME
+    u.lastname = LAST_NAME
+    u.email = EMAIL
+    u.login = LOGIN
+    u.role = ROLE
+    u.set_password(PASSWORD)
+    u.save()
+    return flask_app
+
+
+@pytest.fixture(scope='session')
+def client(flask_app_with_admin):
+    with flask_app_with_admin.test_client() as client:
+        with flask_app_with_admin.app_context():
             yield client
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def app(client):
     app = App(client)
     return app
