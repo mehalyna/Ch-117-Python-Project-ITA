26eb19280ad573eb7320a723b3b439ace363edcd
taras0024 <74822918+taras0024@users.noreply.github.com>
2021-04-16 15:17:52 +0300

    change data for testing update user form

--- admin/tests/test_unit/test_forms/test_update_user_form.py
+++ admin/tests/test_unit/test_forms/test_update_user_form.py
@@ -5,11 +5,11 @@ from admin.forms import UpdateUserForm
 def test_update_user_form_without_password(app):
     app.authorize(LOGIN, PASSWORD)
     data = {
-        'firstname': 'Taras',
-        'lastname': 'Shyichuk',
-        'email': 'taras@ex.com',
-        'login': 'taras0024',
-        'role': 'admin',
+        'firstname': 'TestName',
+        'lastname': 'TestLastname',
+        'email': 'test@ex.com',
+        'login': 'test1234',
+        'role': 'user',
         'status': 'active'
     }
     response = UpdateUserForm(data=data)
