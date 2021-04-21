c65e0aecc385c6f1f770e942d4644d7b72a5aa48
taras0024 <74822918+taras0024@users.noreply.github.com>
2021-04-21 17:23:03 +0300

    created registration form

---
+++ library/catalog/forms.py
@@ -0,0 +1,15 @@
+from django.forms import Form, CharField, EmailField, PasswordInput, TextInput
+
+
+class RegistrationForm(Form):
+    firstname = CharField(label='Firstname', max_length=100,
+                          widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'John'}))
+    lastname = CharField(label='Lastname', max_length=100,
+                         widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'Wick'}))
+    email = EmailField(label='Email', max_length=100,
+                       widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'John@example.com'}))
+    login = CharField(label='Login', max_length=100,
+                      widget=TextInput(attrs={'class': 'form-control', 'placeholder': '_john_1234'}))
+    password = CharField(label='Password', max_length=100, widget=PasswordInput(attrs={'class': 'form-control'}))
+    confirm_password = CharField(label='Confirm password', max_length=100,
+                                 widget=PasswordInput(attrs={'class': 'form-control'}))
--- library/library/settings.py
+++ library/library/settings.py
@@ -20,6 +20,9 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Templates Directory
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -27,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
@@ -58,7 +61,7 @@ ROOT_URLCONF = 'library.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
