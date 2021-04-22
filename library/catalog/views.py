5de2e0a2d90853ae2f223ca20cb0fcc2b2ce5739
Palusha <monday13081@gmail.com>
2021-04-22 17:12:19 +0300

    registration.html refactor

--- library/catalog/views.py
+++ library/catalog/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
-from catalog.forms import RegistrationForm
+from .forms import RegistrationForm
 
 
 def home(request):
