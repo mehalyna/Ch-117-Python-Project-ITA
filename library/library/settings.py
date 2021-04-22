4610a23b8c0612759c986ec2f5ef452ffb71a119
taras0024 <74822918+taras0024@users.noreply.github.com>
2021-04-22 13:57:34 +0300

    refactor folder and change registration UI

--- library/library/settings.py
+++ library/library/settings.py
@@ -20,9 +20,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Templates Directory
-TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -61,7 +58,7 @@ ROOT_URLCONF = 'library.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,6 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
+    '/var/www/static/',
 ]
 
 # Default primary key field type
