22e9ab56674597e61a19d722cdb4338800ca10b5
taras0024 <74822918+taras0024@users.noreply.github.com>
2021-04-22 11:33:46 +0300

    add icons and change placeholder

--- library/catalog/forms.py
+++ library/catalog/forms.py
@@ -7,9 +7,9 @@ class RegistrationForm(Form):
     lastname = CharField(label='Lastname', max_length=100,
                          widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'Wick'}))
     email = EmailField(label='Email', max_length=100,
-                       widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'John@example.com'}))
+                       widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'john@example.com'}))
     login = CharField(label='Login', max_length=100,
-                      widget=TextInput(attrs={'class': 'form-control', 'placeholder': '_john_1234'}))
+                      widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'johny_1234'}))
     password = CharField(label='Password', max_length=100, widget=PasswordInput(attrs={'class': 'form-control'}))
     confirm_password = CharField(label='Confirm password', max_length=100,
                                  widget=PasswordInput(attrs={'class': 'form-control'}))
