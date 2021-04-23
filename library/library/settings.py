962ef5d9b82aaa8ca03031ff9aa27cba6fe3e9fe
Palusha <monday13081@gmail.com>
2021-04-23 12:33:41 +0300

    registration extending base.html

--- library/library/settings.py
+++ library/library/settings.py
@@ -122,7 +122,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    '/var/www/static/',
+]
 
 
 # Default primary key field type
