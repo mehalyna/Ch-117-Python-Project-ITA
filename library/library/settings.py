dacd40832dcbc62651480bfc630f6052e31b6b8c
Andrew Skavreniuk <63035614+AndriiSkavreniuk@users.noreply.github.com>
2021-05-01 23:28:53 +0300

    Update settings.py
    fix debug

--- library/library/settings.py
+++ library/library/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBAG') == 'True'
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
