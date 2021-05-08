96a766a4004985ef007bf53e0dee8ad902038143
taras0024 <74822918+taras0024@users.noreply.github.com>
2021-05-08 11:32:19 +0300

    change password frontend validation

---
+++ library/catalog/static/js/changePasswordForm.js
@@ -0,0 +1,102 @@
+const passPattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+let form = document.querySelector('.needs-validation');
+let inputsList = form.querySelectorAll('input.form-control');
+
+
+let old_password = form.elements.namedItem('old_password');
+let new_password = form.elements.namedItem('new_password');
+let confirmPassword = form.elements.namedItem('confirm_password');
+let submitButton = form.elements.namedItem('submitButton');
+submitButton.disabled = true;
+
+old_password.addEventListener('input', validationFuncsPassword);
+new_password.addEventListener('input', validationFuncsPassword);
+confirmPassword.addEventListener('input', validationFuncsConfirmPassword);
+
+
+function validationFuncsPassword() {
+    validatePassword();
+    checkFormValid();
+}
+
+function validationFuncsConfirmPassword() {
+    validateConfirmPassword();
+    checkFormValid();
+}
+
+
+function validatePassword() {
+    validatePasswords(old_password, '#oldPasswordError');
+    validatePasswords(new_password, '#newPasswordError');
+}
+
+
+function validateConfirmPassword() {
+    validatePasswords(confirmPassword, '#confirmPasswordError');
+}
+
+
+function setErrorFor(errorId, message) {
+    let validationError = document.querySelector(errorId);
+
+    validationError.innerText = message;
+    validationError.style.visibility = 'visible';
+    submitButton.disabled = true;
+}
+
+function setSuccessFor(errorId) {
+    let validationError = document.querySelector(errorId);
+
+    validationError.style.visibility = 'hidden';
+}
+
+function checkFormValid() {
+    let valueInputsList = [];
+    let classInputsList = [];
+
+    inputsList.forEach(input => {
+        classInputsList = classInputsList.concat(input.classList.value.split(' '));
+        valueInputsList.push(input.value);
+    })
+    if (valueInputsList.includes('')) {
+        submitButton.disabled = true;
+    } else if (!classInputsList.includes('is-invalid')) {
+        submitButton.disabled = false;
+    }
+}
+
+function dataRequired(element, errorId) {
+    if (element.value.trim() === '') {
+        element.classList.add('is-invalid');
+        setErrorFor(errorId, 'This field is required');
+        return false;
+    } else {
+        element.classList.remove('is-invalid');
+        setSuccessFor(errorId);
+        return true;
+    }
+}
+
+
+function validatePasswords(element, errorId) {
+    if (!dataRequired(element, errorId)) {
+
+    } else if (!passPattern.test(element.value)) {
+        element.classList.add('is-invalid');
+        setErrorFor(errorId, 'Minimum 8 characters, at least 1 letter and 1 number');
+        return false;
+    } else if (new_password.value !== confirmPassword.value && new_password.value && confirmPassword.value) {
+        confirmPassword.classList.add('is-invalid');
+        setErrorFor('#confirmPasswordError', 'Passwords doesn\'t match');
+        return false;
+    } else if (new_password.value === confirmPassword.value) {
+        confirmPassword.classList.remove('is-invalid');
+        setSuccessFor('#confirmPasswordError');
+        return true;
+    } else {
+        element.classList.remove('is-invalid');
+        setSuccessFor(errorId);
+        return true;
+    }
+}
