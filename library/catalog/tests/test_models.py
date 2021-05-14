ac3036625be05f173d7632147629ed154666baaa
Radik <rodiontulub@gmail.com>
2021-05-14 11:15:32 +0300

    mock mongo db

--- library/catalog/tests/test_models.py
+++ library/catalog/tests/test_models.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from mongoengine import disconnect, connect
+from django.conf import settings
 
 from ..models import MongoUser, DjangoUser
 
@@ -6,6 +8,8 @@ from ..models import MongoUser, DjangoUser
 class MongoUserTest(TestCase):
     @classmethod
     def setUpTestData(cls):
+        disconnect()
+        connect('test', host='mongomock://localhost')
         user = MongoUser()
         user.first_name = 'test'
         user.last_name = 'test'
@@ -14,16 +18,20 @@ class MongoUserTest(TestCase):
         user.password = 'test1234'
         user.save()
 
-    def test_get_user(self):
+    def test_users_creation(self):
         user = MongoUser.objects(username='test_user').first()
+        django_user = DjangoUser.objects.get(username='test_user')
+        self.assertIsInstance(django_user, DjangoUser)
         self.assertIsInstance(user, MongoUser)
 
     def test_get_user1(self):
         user = MongoUser.objects(username='test_user').first()
         self.assertEqual('test_user', user.username)
 
-    def tearDown(self) -> None:
-        MongoUser.objects(username='test_user').first().delete()
+    @classmethod
+    def tearDownClass(cls):
+        disconnect()
+        connect(settings.DB_NAME, host=settings.MONGO_DATABASE_HOST)
 
 
 class DjangoUserTest(TestCase):
