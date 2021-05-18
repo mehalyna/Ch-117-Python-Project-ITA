54089c3582c845abddf5b049c72f3ab19e11f714
Radik <rodiontulub@gmail.com>
2021-05-18 11:26:07 +0300

    tested login view

--- library/catalog/tests/test_views.py
+++ library/catalog/tests/test_views.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from catalog.models import MongoUser
+from ..models import MongoUser
 
 
 class HomePageTest(TestCase):
@@ -42,3 +42,41 @@ class ProfileEditViewTest(TestCase):
         response = self.client.get(reverse('profile_edit'))
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Edit profile', response.content)
+
+
+class LoginViewTest(TestCase):
+    def setUp(self) -> None:
+        user = MongoUser()
+        user.first_name = 'test'
+        user.last_name = 'test'
+        user.username = 'testing'
+        user.email = 'test@gmail.com'
+        user.password = 'test1234'
+        self.user = user.save()
+
+    def tearDown(self) -> None:
+        self.user.delete()
+
+    def test_login_with_valid_data(self):
+        data = {
+            'username': 'testing',
+            'password': 'test1234'
+        }
+        response = self.client.post('/library/func_login', data=data)
+        self.assertIn(b'Success', response.content)
+
+    def test_login_with_invalid_username(self):
+        data = {
+            'username': 'not_exist_username',
+            'password': 'test1234'
+        }
+        response = self.client.post('/library/func_login', data=data)
+        self.assertIn(b'Denied', response.content)
+
+    def test_login_with_invalid_password(self):
+        data = {
+            'username': 'testing',
+            'password': 'invalid_pass'
+        }
+        response = self.client.post('/library/func_login', data=data)
+        self.assertIn(b'Denied', response.content)
