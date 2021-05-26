c22a4316d3db33b86c5aa31a648a5c7365d9f275
taras0024 <74822918+taras0024@users.noreply.github.com>
2021-05-26 16:56:45 +0300

    admin - update user password fix

--- admin/app.py
+++ admin/app.py
@@ -130,15 +130,19 @@ def create_app():
                 lastname = form.lastname.data
                 email = form.email.data
                 login = form.login.data
-                password_hash = make_password(form.password.data)
                 role = form.role.data
                 is_admin = form.role.data == Role.ADMIN
                 status = form.status.data
+                password_data = form.password.data
+                if password_data:
+                    password = make_password(password_data)
+                else:
+                    password = user.password
                 if _id == session.get('_user_id') and (status != Status.ACTIVE or role != Role.ADMIN):
                     flash('The administrator cannot change the status or role for himself', 'warning')
                 else:
                     user.update(firstname=firstname, lastname=lastname,
-                                email=email, username=login, password=password_hash,
+                                email=email, username=login, password=password,
                                 role=role, status=status, is_admin=is_admin)
                     flash('User successfully updated', 'success')
                 return redirect(utils.back_to_page('page', 'userSearch', 'urlPath'))
